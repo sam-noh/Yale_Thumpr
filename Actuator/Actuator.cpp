0bbe35682bfc5d9ef07c1d0b09e0524eb611d4be
Seonghoon Noh <seonghoon.noh@yale.edu>
2023-09-13 10:42:19 -0400

    Pushing development changes over several months. See readme for updates

--- Actuator/Actuator.cpp
+++ Actuator/Actuator.cpp
@@ -1,23 +1,28 @@
 #include "Actuator.h"
 
 // alternate constructor
-Actuator::Actuator(const ODriveTeensyCAN & _ODrive, usb_serial_class & _USB_Serial, uint8_t _axis, float _torqueConstant, float _minPos, float _maxPos): myODrive(_ODrive), myUSBSerial(_USB_Serial) {
-    states.axis = _axis;
+Actuator::Actuator(const ODriveTeensyCAN & _ODrive, usb_serial_class & _USB_Serial, uint8_t _axis, int _direction, float _T, float _K_T, float _qMin, float _qMax): myODrive(_ODrive), myUSBSerial(_USB_Serial) {
     states.homed = false;
+    states.holding = false;
     states.state = 0;
+    states.ctrl_mode = 0;
     states.pos_home = 0;
-    states.pos_min = _minPos;
-    states.pos_max = _maxPos;
-    params.torqueConstant = _torqueConstant;
+    states.q_min = _qMin;
+    states.q_max = _qMax;
+    params.axis = _axis;
+    params.direction = _direction;
+    params.T = _T;
+    params.K_T = _K_T;
 }
 
 bool Actuator::enable() {
-    bool success = myODrive.RunState(states.axis, 8);
+    bool success = myODrive.RunState(params.axis, 8);
     if (success) {
-        snprintf(sentData, sizeof(sentData), "Actuator %d entered closed-loop control.\n", states.axis+1);
+        states.state = 8;
+        snprintf(sentData, sizeof(sentData), "Actuator %d entered closed-loop control.\n", params.axis+1);
         myUSBSerial.print(sentData);
     } else {
-        snprintf(sentData, sizeof(sentData), "Actuator %d failed to enter closed-loop control.\n", states.axis+1);
+        snprintf(sentData, sizeof(sentData), "Actuator %d failed to enter closed-loop control.\n", params.axis+1);
         myUSBSerial.print(sentData);
     }
     return success;
@@ -25,36 +30,43 @@ bool Actuator::enable() {
 
 bool Actuator::disable() {
     if (states.state != 1) {
-        bool success = myODrive.RunState(states.axis, 1);
+        bool success = myODrive.RunState(params.axis, 1);
         if (success) {
-            snprintf(sentData, sizeof(sentData), "Actuator %d entered idle mode.\n", states.axis+1);
+            states.state = 1;
+            snprintf(sentData, sizeof(sentData), "Actuator %d entered idle mode.\n", params.axis+1);
             myUSBSerial.print(sentData);
         } else {
-            snprintf(sentData, sizeof(sentData), "Actuator %d failed to enter idle mode.\n", states.axis+1);
+            snprintf(sentData, sizeof(sentData), "Actuator %d failed to enter idle mode.\n", params.axis+1);
             myUSBSerial.print(sentData);
         }
         return success;
         
     } else {
-        snprintf(sentData, sizeof(sentData), "Actuator %d already in idle.\n", states.axis+1);
+        snprintf(sentData, sizeof(sentData), "Actuator %d already in idle.\n", params.axis+1);
         myUSBSerial.print(sentData);
         return true;
     }
 }
 
 void Actuator::sendCommand(uint8_t mode, float val) {
+    float cmd = 0;
+    if (mode == 1) {    // for now, torque command is NOT in joint space
+        cmd = params.direction*val;
+    } else {
+        cmd = (params.direction*val)/params.T;
+    }
+    
     if (mode == 1) {
-        myODrive.SetTorque(states.axis, val);
+        myODrive.SetTorque(params.axis, cmd);
     } else if (mode == 2) {
-        myODrive.SetVelocity(states.axis, val);
+        myODrive.SetVelocity(params.axis, cmd);
     } else if (mode == 3) {
-        if (val >= states.pos_min && val <= states.pos_max) { // checking bounds on pos_rel
-            myODrive.SetPosition(states.axis, val  + states.pos_home); // converts to and sends pos_abs as the position command to the ODrive
+        if (!states.homed || states.homed && val >= states.q_min && val <= states.q_max) { // ONLY CHECK BOUNDS IF HOMED; checking bounds on pos_rel
+            myODrive.SetPosition(params.axis, cmd  + states.pos_home); // converts to and sends pos_abs as the position command to the ODrive
         } else {
-            snprintf(sentData, sizeof(sentData), "Actuator %d position setpoint outside of range.\n", states.axis+1);
-            // Serial.printf("%.3f\t%.3f\t%.3f\n", val, )
-            // delay(100000);
+            snprintf(sentData, sizeof(sentData), "Actuator %d position setpoint outside of range.\n", params.axis+1);
             myUSBSerial.print(sentData);
+            Serial.printf("Min: %.3f\tMax: %.3f\tq:%.3f\n", states.q_min, states.q_max, val);
         }
         
     } else {
@@ -65,12 +77,19 @@ void Actuator::sendCommand(uint8_t mode, float val) {
 
 void Actuator::setControlMode(uint8_t mode) {
     if (mode == 3) {
-        myODrive.SetControllerModes(states.axis, 3, 5);
+        myODrive.SetControllerModes(params.axis, 3, 5);     // 3 = pos ctrl, 5 = trapezoidal trajectory
+        states.ctrl_mode = 3;
         snprintf(sentData, sizeof(sentData), "Entering position control.\n");
         myUSBSerial.print(sentData);
-    } else if (mode == 1 || mode == 2) {
-        myODrive.SetControllerModes(states.axis, mode, 1);
+    } else if (mode == 2) {
+        myODrive.SetControllerModes(params.axis, 2, 1);  // 2 = vel ctrl, 1 = passthrough input
+        states.ctrl_mode = 2;
         snprintf(sentData, sizeof(sentData), "Entering velocity control.\n");
+        myUSBSerial.print(sentData);
+    } else if (mode  == 1) {
+        myODrive.SetControllerModes(params.axis, 1, 1);  // 1 = torque ctrl, 1 = passthrough input
+        states.ctrl_mode = 1;
+        snprintf(sentData, sizeof(sentData), "Entering torque control.\n");
         myUSBSerial.print(sentData);
     } else {
         snprintf(sentData, sizeof(sentData), "Invalid control mode.\n");
--- Actuator/Actuator.h
+++ Actuator/Actuator.h
@@ -10,27 +10,39 @@ class Actuator {
   // all position commands are converted to pos_abs by subtracting pos_home (when not homed, pos_home = 0)
   // e.g. if pos_home is -3 and the position command is 50, the actuator will move to pos_abs = 53
   struct axisStates{
-    uint8_t axis;           // CAN node id of the control axis (odrv<>.axis<>.config.can.node_id)
     uint8_t state;          // ODrive axis current state (odrv<>.axis<>.current_state)
+    uint8_t ctrl_mode;      // ODrive axis control mdoe (odrv<>.axis<>.controller.config.control_mode)
     uint32_t axisError;     // ODrive axis error (odrv<>.axis<>.error)
-    bool homed;             // false initially; calling setHomePosition() sets it to true
+    bool homed;             // false until homed and manually to true
+    bool holding;           // true if fabs(q-q_d) < Q_MAX_ERROR
 
+    // "actuator space" values
     float pos_abs;          // motor position reported by the ODrive after powerup (turns)
     float pos_rel;          // pos_rel = pos_abs - pos_home (turns)
-    float pos_home;         // pos_abs at home; call setHomePosition()
-    float pos_min;          // minimum allowed pos_rel
-    float pos_max;          // maximum allowed pos_rel
-    float velocity;         // motor velocity (turns/s)
+    float pos_home;         // pos_abs at home
+    float vel;              // motor velocity (turns/s)
     float current;          // motor current draw (amps)
     float torque;           // motor torque (Nm)
 
-    float q_cur;            // joint position in mm or rad (transmission ratio*pos_rel)
-    float q_target;         // joint setpoint in mm or rad
+    // joint space values
+    float q;                // joint position in mm or deg (transmission ratio*pos_rel)
+    float q_d;              // joint position setpoint in mm or deg
+    float q_min;            // minimum allowed q
+    float q_max;            // maximum allowed q
+
+    float q_dot;            // joint velocity in mm/s or deg/s (transmission ratio*vel)
+    float q_dot_d;          // joint velocity setpoint in mm/s or deg/s
+
+    float tau;              // joint force/torque in F or Nm (equal to motor torque for now)
+    float tau_d;            // joint force/torque setpoint in F or Nm
   };
 
   // constant values
   struct axisParams{
-    float torqueConstant;   // motor torque constant
+    uint8_t axis;           // CAN node id of the control axis (odrv<>.axis<>.config.can.node_id)
+    int direction;          // actuator motion direction; 1 or -1
+    float K_T;              // motor torque constant
+    float T;                // transmission ratio = joint disp./motor shaft disp.
   };
 
   private:
@@ -44,7 +56,7 @@ class Actuator {
     struct axisParams params;
 
     // alternate constructor
-    Actuator(const ODriveTeensyCAN & _ODrive, usb_serial_class & _USB_Serial,  uint8_t _axis, float _torqueConstant, float _minPos, float _maxPos);
+    Actuator(const ODriveTeensyCAN & _ODrive, usb_serial_class & _USB_Serial,  uint8_t _axis, int _direction, float _transmissionRatio, float _torqueConstant, float _minPos, float _maxPos);
 
     bool enable();                // enters closed-loop control
     bool disable();               // enters idle state
