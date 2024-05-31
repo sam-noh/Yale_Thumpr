e696260b89353d2132803dd971f638c8be1c4426
Seonghoon Noh <seonghoon.noh@yale.edu>
2024-05-31 15:41:21 -0400

    Parameter change

--- src/state_estimation/state_estimation.h
+++ src/state_estimation/state_estimation.h
@@ -57,7 +57,7 @@ const int kTorqueFilterLength = 3;          // number of torque samples to hold 
 const int kLegVelFilterLength = 3;          // number of leg velocity samples to hold in the FIFO queue
 
 // leg acceleration moving average filter
-const int kLegAccelFilterLength = 4;        // number of leg acceleration samples to hold in the FIFO queue
+const int kLegAccelFilterLength = 6;        // number of leg acceleration samples to hold in the FIFO queue
 
 // IMU parameters
 const int kIMUAvgSize = 100;                                // IMU pose estimation initial value average sample size
@@ -71,7 +71,7 @@ const std::vector<int> kBodyFrameAxisIndex = {2, -1, 3};    // IMU frame to body
 // contact detection
 const float kDqLegMotorStartContact = kQLegUnstuck;     // leg displacment in mm past which contact detection begins; this value MUST BE less than the leg retraction amount (see leg_swing_percent)
 const float kQdotLegContact = 5;                        // leg touchdown velocity in mm/s below which contact is likely
-const float kQddotLegContact = -1700;                   // leg acceleration in mm/s^2 above which (more negative) leg contact is likely
+const float kQddotLegContact = -2000;                   // leg acceleration in mm/s^2 above which (more negative) leg contact is likely
 const float kQdotLegPercentContact = 0.3;               // percentage of max leg touchdown velocity at which ground contact is assumed
                                                         // this method seeks to detect contact sooner than standstill by checking for velocity reduction
 
